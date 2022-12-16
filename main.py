38127a6c3aa1a40eb3a316c95d51efce391c88cc
cottierguillya <cottierguillya@educ.college-montreal.qc.ca>
2022-12-16 16:21:46 -0500

    oui

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import arcade, random, math
+import arcade, random, math, opensimplex
 
 # Constants
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+BLOCKS_SIZE = (SCREEN_WIDTH//35,)*2
+
+map_noise = opensimplex.OpenSimplex(random.randint(0, 1_000_000_000_000_000_000))
 
 def get_scale_with_size(width, height, sprite):
     return min(width / sprite.width, height / sprite.height)
@@ -15,18 +18,14 @@ def get_map(width, height):
     for i in range(height):
         map.append([])
         for j in range(width):
-            prob = random.random()
-            try:
-                if j != 0 and map[i][j - 1] == 1:
-                    prob = 0.75
-                if i != 0 and map[i - 1][j] == 1:
-                    prob += 0.2
-            except IndexError:
-                pass
-            if random.random() > prob:
-                map[i].append(1)
+            prob = map_noise.noise2(i/5, j/5)
+            if random.random() < 0.005:
+                map[i].append(2)
             else:
-                map[i].append(0)
+                if prob < 0.2:
+                    map[i].append(0)
+                elif prob < 0.8:
+                    map[i].append(1)
     return map
 
 
@@ -58,10 +57,11 @@ class Wall(arcade.Sprite):
 
 # Create a golf hole
 class Hole(arcade.Sprite):
-    def __init__(self):
+    def __init__(self, x, y, scale):
         super().__init__("C:/Users/cottierguillya/PycharmProjects/Etape_1/copilot_tests2/assets/hole.png", 0.5)
-        self.center_x = 500
-        self.center_y = 500
+        self.center_x = x
+        self.center_y = y
+        self.scale = scale
 
     def update(self):
         pass
@@ -73,45 +73,50 @@ class GolfGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Golf Game")
         arcade.set_background_color(arcade.color.AMAZON)
         self.ball = Ball()
-        self.ball.scale = get_scale_with_size(SCREEN_WIDTH / 35, SCREEN_HEIGHT / 35, self.ball)
-        self.hole = Hole()
-        self.hole.scale = get_scale_with_size(SCREEN_WIDTH / 20, SCREEN_HEIGHT / 20, self.hole)
+        self.ball.scale = get_scale_with_size(SCREEN_WIDTH / 100, SCREEN_HEIGHT / 100, self.ball)
+        self.holes = arcade.SpriteList()
 
         self.walls = arcade.SpriteList()
-        self.load_map(get_map(round(SCREEN_WIDTH / 25), round(SCREEN_HEIGHT / 30)))
+        self.load_map(get_map(round(SCREEN_WIDTH/BLOCKS_SIZE[0] + 0.499999999), round(SCREEN_HEIGHT/BLOCKS_SIZE[1] + 0.499999999)))
 
     def load_map(self, map):
         for i in range(len(map)):
             for j in range(len(map[i])):
+                print(map[i][j])
                 if map[i][j] == 1:
-                    wall = Wall(j * 20, i * 20, 0.5)
-                    wall.scale = get_scale_with_size(SCREEN_WIDTH / 25, SCREEN_HEIGHT / 30, wall)
+                    wall = Wall(j * BLOCKS_SIZE[0], i * BLOCKS_SIZE[1], 0.5)
+                    wall.scale = get_scale_with_size(*BLOCKS_SIZE, wall)
                     self.walls.append(wall)
+                elif map[i][j] == 2:
+                    hole = Hole(j * BLOCKS_SIZE[0], i * BLOCKS_SIZE[1], 0.5)
+                    hole.scale = get_scale_with_size(*BLOCKS_SIZE, hole)
+                    self.holes.append(hole)
 
     def on_draw(self):
         arcade.start_render()
         self.ball.draw()
-        self.hole.draw()
+        self.holes.draw()
         self.walls.draw()
 
     def update(self, delta_time):
         # Deal ball collision with screen edges
-        self.hole.update()
+        for hole in self.holes:
+            hole.update()
         self.walls.update()
 
-        if SCREEN_WIDTH < self.ball.center_x + self.ball.width / 2:
-            self.ball.velocity[0] *= -1
+        # Collision with screen edges
+        if SCREEN_WIDTH < self.ball.center_x - self.ball.width / 2:
             self.ball.center_x = SCREEN_WIDTH - self.ball.width / 2
-        elif self.ball.center_x - self.ball.width / 2 < 0:
             self.ball.velocity[0] *= -1
+        elif self.ball.center_x - self.ball.width / 2 < 0:
             self.ball.center_x = self.ball.width / 2
-
-        if SCREEN_HEIGHT < self.ball.center_y + self.ball.height / 2:
-            self.ball.velocity[1] *= -1
+            self.ball.velocity[0] *= -1
+        if SCREEN_HEIGHT < self.ball.center_y - self.ball.height / 2:
             self.ball.center_y = SCREEN_HEIGHT - self.ball.height / 2
-        elif self.ball.center_y - self.ball.height / 2 < 0:
             self.ball.velocity[1] *= -1
+        elif self.ball.center_y - self.ball.height / 2 < 0:
             self.ball.center_y = self.ball.height / 2
+            self.ball.velocity[1] *= -1
 
         # Deal ball collision with walls
         for wall in self.walls:
@@ -125,9 +130,10 @@ class GolfGame(arcade.Window):
                 self.ball.center_y += self.ball.velocity[1] * self.ball.speed
 
         # Deal ball collision with hole
-        if arcade.check_for_collision(self.ball, self.hole):
-            print("You win!")
-            arcade.close_window()
+        for hole in self.holes:
+            if arcade.check_for_collision(self.ball, hole):
+                print("You win!")
+                arcade.close_window()
 
         self.ball.update()
 
